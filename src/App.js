6c0886ba718948fee8f3e472a2ebe14b433dfd0b
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-02-15 13:48:30 +0000

    router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages";
+import {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  SingleProducts,
+  Product,
+} from "./pages";
 
 function App() {
   return (
-    <div className="container">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/product">
+          <Product />
+        </Route>
+        <Route exact="/product/:id" children={<SingleProducts />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/error">
+          <Error />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoutePage";
+import SingleProducts from "./SingleProductPage";
+import Product from "./ProductPage";
+
+export {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  SingleProducts,
+  Product,
+};
